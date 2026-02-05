c23e99c5a50bf86fdf3c08954d93fb0fbe884f90
Abel Mekonnen <abelmekonen247@gmail.com>
2026-02-05 11:10:59 +0300

    Update solution for 2491-smallest-even-multiple/solution.py

--- 2491-smallest-even-multiple/solution.py
+++ 2491-smallest-even-multiple/solution.py
@@ -1,3 +1,8 @@
 class Solution:
     def smallestEvenMultiple(self, n: int) -> int:
-        
+        i = n
+        while True:
+            if i % 2 == 0:
+                return i
+            i += n
+
